529943aa782787920075cb650372bda68d7044b2
exydos222 <56573918+exydos222@users.noreply.github.com>
2023-01-08 17:41:21 -0700

    Remove all instances of allocator, needs rewrite

--- kernel.c
+++ kernel.c
@@ -7,8 +7,6 @@
 void kernel_main()
 {
 	setup_allocator();
-	current_terminal_input = (char*)allocate(1);
-	current_directory = (char*)allocate(1);
 	current_directory[0] = '\0';
 	setup_gdt();
 	setup_idt();
--- output.h
+++ output.h
@@ -43,6 +43,8 @@
 
 #define VIDEO_MEMORY_ADDRESS ((char*)0xB8000)
 
+#define MAX_INPUT 256
+
 #define toggle_insert_mode() enable_terminal_cursor((insert_mode = !insert_mode))
 
 char* current_terminal_input;
@@ -462,6 +464,8 @@ void simulate_terminal_keystroke(const char key) {
     unsigned short length = string_length(current_terminal_input);
     current_terminal_input = (char*)reallocate(current_terminal_input, length + 1, length + 2);
     const unsigned short pos = get_cursor_position() * 2;
+    if ((pos - USER_POS) / 2 == MAX_INPUT - 1)
+        return;
     if (insert_mode == true && CURSOR_OFFSET != 0) {
         current_terminal_input[(pos - USER_POS) / 2] = key;
         VIDEO_MEMORY_ADDRESS[pos] = key;
@@ -469,7 +473,7 @@ void simulate_terminal_keystroke(const char key) {
     } else {
         string_push(current_terminal_input, key, (pos - USER_POS) / 2);
         TEXT_POS-=CURSOR_OFFSET * 2;
-        char* update = (char*)allocate(length + 2);
+        char update[MAX_INPUT];
         copy_string(update, current_terminal_input);
         substring_one(update, (pos - USER_POS) / 2);
         print(update);
@@ -478,6 +482,8 @@ void simulate_terminal_keystroke(const char key) {
 
 void simulate_terminal_keystroke_colored(const char key, const unsigned short color_code) {
     const unsigned short pos = get_cursor_position() * 2;
+    if ((pos - USER_POS) / 2 == MAX_INPUT - 1)
+        return;
     if (insert_mode == true && CURSOR_OFFSET != 0) {
         current_terminal_input[(pos - USER_POS) / 2] = key;
         VIDEO_MEMORY_ADDRESS[pos] = key;
@@ -485,7 +491,7 @@ void simulate_terminal_keystroke_colored(const char key, const unsigned short co
     } else {
         string_push(current_terminal_input, key, (pos - USER_POS) / 2);
         TEXT_POS-=CURSOR_OFFSET * 2;
-        char update[256];
+        char update[MAX_INPUT];
         copy_string(update, current_terminal_input);
         substring_one(update, (pos - USER_POS) / 2);
         print_colored(update, color_code);
@@ -501,7 +507,7 @@ void simulate_terminal_backspace() {
         VIDEO_MEMORY_ADDRESS[TEXT_POS - 2] = 0;
         VIDEO_MEMORY_ADDRESS[TEXT_POS - 1] = 7;
         TEXT_POS-=CURSOR_OFFSET * 2;
-        char update[256];
+        char update[MAX_INPUT];
         copy_string(update, current_terminal_input);
         substring_one(update, (pos - USER_POS) / 2);
         print(update);
@@ -512,7 +518,7 @@ void simulate_terminal_backspace() {
         VIDEO_MEMORY_ADDRESS[TEXT_POS - 2] = 0;
         VIDEO_MEMORY_ADDRESS[TEXT_POS - 1] = 7;
         TEXT_POS-=(CURSOR_OFFSET + 1) * 2;
-        char update[256];
+        char update[MAX_INPUT];
         copy_string(update, current_terminal_input);
         substring_one(update, (pos - USER_POS) / 2 - 1);
         print(update);
@@ -550,7 +556,7 @@ bool simulate_terminal_command(const char* command) {
         execute_file(command);
         return false;
     }
-    char path[256];
+    char path[MAX_PATH + MAX_INPUT + 1];
     copy_string(path, current_directory);
     concatenate_char_to_string(path, '/');
     concatenate_string_to_string(path, command);
