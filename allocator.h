ca63d1c91ecc097db1c45cefafb6f2ccafffb705
exydos222 <56573918+exydos222@users.noreply.github.com>
2023-01-11 18:33:25 -0700

    Rename block to memory_block

--- allocator.h
+++ allocator.h
@@ -1,86 +1,86 @@
 #pragma once
 
-#define MEMORY_BLOCK_SIZE 4096
-#define MEMORY_BLOCK_DATA_TYPE unsigned short
+#define memory_block_SIZE 4096
+#define memory_block_DATA_TYPE unsigned short
 
 struct memory_block {
     void* data_pointer;
-    MEMORY_BLOCK_DATA_TYPE used;
+    memory_block_DATA_TYPE used;
     struct memory_block* next;
 };
 
-struct memory_block* first_block;
+struct memory_block* first_memory_block;
 
 void setup_allocator() {
-    static char data[MEMORY_BLOCK_SIZE];
-    static struct memory_block new_block;
-    new_block.data_pointer = &data;
-    first_block = &new_block;
+    static char data[memory_block_SIZE];
+    static struct memory_block new_memory_block;
+    new_memory_block.data_pointer = &data;
+    first_memory_block = &new_memory_block;
 }
 
-const void* allocate(const MEMORY_BLOCK_DATA_TYPE size) {
-    if (size > MEMORY_BLOCK_SIZE)
+const void* allocate(const memory_block_DATA_TYPE size) {
+    if (size > memory_block_SIZE)
         return 0;
-    for (struct memory_block* block = first_block; block; block = block->next)
-        if (block->used + size > MEMORY_BLOCK_SIZE) {
-            if (block->next)
+    for (struct memory_block* memory_block = first_memory_block; memory_block; memory_block = memory_block->next)
+        if (memory_block->used + size > memory_block_SIZE) {
+            if (memory_block->next)
                 continue;
-            static char data[MEMORY_BLOCK_SIZE];
-            static struct memory_block new_block;
-            new_block.data_pointer = &data;
-            block->next = &new_block;
-            block->next->used+=size;
-            return block->next->data_pointer;
+            static char data[memory_block_SIZE];
+            static struct memory_block new_memory_block;
+            new_memory_block.data_pointer = &data;
+            memory_block->next = &new_memory_block;
+            memory_block->next->used+=size;
+            return memory_block->next->data_pointer;
         } else {
-            const void* address = &((char*)block->data_pointer)[block->used];
-            block->used+=size;
+            const void* address = &((char*)memory_block->data_pointer)[memory_block->used];
+            memory_block->used+=size;
             return address;
         }
     return 0;
 }
 
-void deallocate(const void* address, const MEMORY_BLOCK_DATA_TYPE size) {
-    for (struct memory_block* block = first_block; block; block = block->next)
-        if ((unsigned int)address >= (unsigned int)block->data_pointer && (unsigned int)address <= (unsigned int)block->data_pointer + MEMORY_BLOCK_SIZE) {
-            for (MEMORY_BLOCK_DATA_TYPE i = (unsigned int)address - (unsigned int)block->data_pointer + size; i < block->used; i++)
-                ((char*)block->data_pointer)[i - size] = ((char*)block->data_pointer)[i];
-            for (MEMORY_BLOCK_DATA_TYPE i = 0; i < size; i++)
-                ((char*)block->data_pointer)[block->used - i - 1] = 0;
-            block->used -= size;
+void deallocate(const void* address, const memory_block_DATA_TYPE size) {
+    for (struct memory_block* memory_block = first_memory_block; memory_block; memory_block = memory_block->next)
+        if ((unsigned int)address >= (unsigned int)memory_block->data_pointer && (unsigned int)address <= (unsigned int)memory_block->data_pointer + memory_block_SIZE) {
+            for (memory_block_DATA_TYPE i = (unsigned int)address - (unsigned int)memory_block->data_pointer + size; i < memory_block->used; i++)
+                ((char*)memory_block->data_pointer)[i - size] = ((char*)memory_block->data_pointer)[i];
+            for (memory_block_DATA_TYPE i = 0; i < size; i++)
+                ((char*)memory_block->data_pointer)[memory_block->used - i - 1] = 0;
+            memory_block->used -= size;
         }
 }
 
-const void* reallocate(const void* address, const MEMORY_BLOCK_DATA_TYPE old_size, const MEMORY_BLOCK_DATA_TYPE new_size) {
-    if (new_size > MEMORY_BLOCK_SIZE)
+const void* reallocate(const void* address, const memory_block_DATA_TYPE old_size, const memory_block_DATA_TYPE new_size) {
+    if (new_size > memory_block_SIZE)
         return 0;
-    for (struct memory_block* block = first_block; block; block = block->next)
-        if ((unsigned int)address >= (unsigned int)block->data_pointer && (unsigned int)address <= (unsigned int)block->data_pointer + MEMORY_BLOCK_SIZE)
+    for (struct memory_block* memory_block = first_memory_block; memory_block; memory_block = memory_block->next)
+        if ((unsigned int)address >= (unsigned int)memory_block->data_pointer && (unsigned int)address <= (unsigned int)memory_block->data_pointer + memory_block_SIZE)
             if (old_size >= new_size) {
-                for (MEMORY_BLOCK_DATA_TYPE i = (unsigned int)address - (unsigned int)block->data_pointer + old_size; i < block->used; i++)
-                    ((char*)block->data_pointer)[i - old_size + new_size] = ((char*)block->data_pointer)[i];
-                for (MEMORY_BLOCK_DATA_TYPE i = 0; i < old_size - new_size; i++)
-                    ((char*)block->data_pointer)[block->used - i - 1] = 0;
-                block->used-=old_size - new_size;
+                for (memory_block_DATA_TYPE i = (unsigned int)address - (unsigned int)memory_block->data_pointer + old_size; i < memory_block->used; i++)
+                    ((char*)memory_block->data_pointer)[i - old_size + new_size] = ((char*)memory_block->data_pointer)[i];
+                for (memory_block_DATA_TYPE i = 0; i < old_size - new_size; i++)
+                    ((char*)memory_block->data_pointer)[memory_block->used - i - 1] = 0;
+                memory_block->used-=old_size - new_size;
                 return address;
             } else {
-                if (block->used - old_size + new_size > MEMORY_BLOCK_SIZE) {
+                if (memory_block->used - old_size + new_size > memory_block_SIZE) {
                     address = allocate(new_size);
-                    const MEMORY_BLOCK_DATA_TYPE old_end = (unsigned int)address - (unsigned int)block->data_pointer + old_size;
-                    for (MEMORY_BLOCK_DATA_TYPE i = old_end; i < block->used; i++) {
-                        ((char*)address)[i - old_end] = ((char*)block->data_pointer)[i - old_size];
-                        ((char*)block->data_pointer)[i - old_size] = ((char*)block->data_pointer)[i];
+                    const memory_block_DATA_TYPE old_end = (unsigned int)address - (unsigned int)memory_block->data_pointer + old_size;
+                    for (memory_block_DATA_TYPE i = old_end; i < memory_block->used; i++) {
+                        ((char*)address)[i - old_end] = ((char*)memory_block->data_pointer)[i - old_size];
+                        ((char*)memory_block->data_pointer)[i - old_size] = ((char*)memory_block->data_pointer)[i];
                     }
-                    for (MEMORY_BLOCK_DATA_TYPE i = 0; i < old_size; i++)
-                        ((char*)block->data_pointer)[block->used - i - 1] = 0;
-                    block->used -= old_size;
+                    for (memory_block_DATA_TYPE i = 0; i < old_size; i++)
+                        ((char*)memory_block->data_pointer)[memory_block->used - i - 1] = 0;
+                    memory_block->used -= old_size;
                     return address;
                 } else {
-                    const MEMORY_BLOCK_DATA_TYPE old_end = (unsigned int)address - (unsigned int)block->data_pointer + old_size;
-                    for (MEMORY_BLOCK_DATA_TYPE i = old_end; i < block->used; i++)
-                        ((char*)block->data_pointer)[i + new_size - old_size] = ((char*)block->data_pointer)[i];
-                    for (MEMORY_BLOCK_DATA_TYPE i = 0; i < new_size - old_size; i++)
-                        ((char*)block->data_pointer)[old_end + i] = 0;
-                    block->used += new_size - old_size;
+                    const memory_block_DATA_TYPE old_end = (unsigned int)address - (unsigned int)memory_block->data_pointer + old_size;
+                    for (memory_block_DATA_TYPE i = old_end; i < memory_block->used; i++)
+                        ((char*)memory_block->data_pointer)[i + new_size - old_size] = ((char*)memory_block->data_pointer)[i];
+                    for (memory_block_DATA_TYPE i = 0; i < new_size - old_size; i++)
+                        ((char*)memory_block->data_pointer)[old_end + i] = 0;
+                    memory_block->used += new_size - old_size;
                     return address;
                 }
             }
